f48e2bbcacae207ca4c8fe753d6c667281f11756
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2021-11-29 18:57:07 -0300

    Initial Commit

---
+++ src/Models/EncryptableModel.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace WellingtonCarneiroBarbosa\EncryptDatabase\Traits;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+use WellingtonCraneiroBarbosa\EncryptDatabase\Encrypter;
+
+class EncryptableModel extends EloquentModel
+{
+    use HasEncryptedAttributes;
+
+    /**
+     * If attribute mutate should be skipped
+     *
+     * @var boolean
+     */
+    public $preventAttrMutator = false;
+
+     /**
+     * If attribute getter should be skipped
+     *
+     * @var boolean
+     */
+    public $preventAttrGetter = false;
+
+    /**
+     * If should enable encryptation
+     *
+     * @var boolean
+     */
+    public $enableEncryption = true;
+
+    /**
+     * The attributes that should be encrypteds.
+     *
+     * @var array
+     */
+    protected $encryptable = [];
+
+    /**
+     * @return array
+     */
+    public function getEncryptableAttributes()
+    {
+        return $this->encryptable;
+    }
+
+    public function isEncryptable($attribute_key) {
+        if($this->enableEncryption){
+            return in_array($attribute_key, $this->encryptable);
+        }
+
+        return false;
+    }
+
+    public function decrypt($attribute_key, $value)
+    {
+        if($this->isEncryptable($attribute_key)) {
+            if($value && (!is_null($value)) && $value != '') {
+                $decrypted = Encrypter::decrypt($value);
+
+                if(! $decrypted) {
+                    return $value;
+                }
+
+                return $decrypted;
+            }
+        }
+
+        return $value;
+    }
+
+    public function encrypt($value)
+    {
+        if($value && (!is_null($value)) && $value != '') {
+            return Encrypter::encrypt($value);
+        }
+
+        return $value;
+    }
+
+    public function encryptAttributesOnCreating()
+    {
+        if($this->enableEncryption) {
+            $attributes = $this->getEncryptableAttributes();
+
+            foreach($attributes as $att) {
+                $value = $this->attributes[$att] ?? '';
+
+                if($value && (!is_null($value)) && $value != '') {
+                    $this->preventAttrMutator = true;
+                    $this->$att = $this->encrypt($value);
+                }
+            }
+        }
+    }
+
+    public function encryptAttributesOnUpdating()
+    {
+        if($this->enableEncryption) {
+            $attributes = $this->getEncryptableAttributes();
+
+            foreach($attributes as $att) {
+                if($this->isDirty($att)){
+                    // original modified attribute
+                    $value = $this->attributes[$att] ?? '';
+
+                    if($value && (!is_null($value)) && $value != '') {
+                        $this->preventAttrMutator = true;
+                        $this->$att = $this->encrypt($value);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ src/Providers/EncryptDatabaseProvider.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace WellingtonCarneiroBarbosa\EncryptDatabase\Providers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
+
+use ESolution\DBEncryption\Console\Commands\EncryptModel;
+use ESolution\DBEncryption\Console\Commands\DecryptModel;
+
+class EncryptDatabaseProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * This method is called after all other service providers have
+     * been registered, meaning you have access to all other services
+     * that have been registered by the framework.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->bootValidators();
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                EncryptModel::class,
+                DecryptModel::class
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+
+
+    private function bootValidators()
+    {
+
+        Validator::extend('unique_encrypted', function ($attribute, $value, $parameters, $validator) {
+
+            // Initialize
+            $salt = substr(hash('sha256', env('APP_KEY')), 0, 16);
+
+            $withFilter = count($parameters) > 3 ? true : false;
+
+            $ignore_id = isset($parameters[2]) ? $parameters[2] : '';
+
+            // Check using normal checker
+            $data = DB::table($parameters[0])->whereRaw("CONVERT(AES_DECRYPT(FROM_bASE64(`{$parameters[1]}`), '{$salt}') USING utf8mb4) = '{$value}' ");
+            $data = $ignore_id != '' ? $data->where('id','!=',$ignore_id) : $data;
+
+            if ($withFilter) {
+                $data->where($parameters[3], $parameters[4]);
+            }
+
+            if($data->first()){
+                return false;
+            }
+
+            return true;
+        });
+
+        Validator::extend('exists_encrypted', function ($attribute, $value, $parameters, $validator) {
+
+            // Initialize
+            $salt = substr(hash('sha256', env('APP_KEY')), 0, 16);
+
+            $withFilter = count($parameters) > 3 ? true : false;
+            if(!$withFilter){
+                $ignore_id = isset($parameters[2]) ? $parameters[2] : '';
+            }else{
+                $ignore_id = isset($parameters[4]) ? $parameters[4] : '';
+            }
+            
+            // Check using normal checker
+            $data = DB::table($parameters[0])->whereRaw("CONVERT(AES_DECRYPT(FROM_bASE64(`{$parameters[1]}`), '{$salt}') USING utf8mb4) = '{$value}' ");
+            $data = $ignore_id != '' ? $data->where('id','!=',$ignore_id) : $data;
+
+            if ($withFilter) {
+                $data->where($parameters[2], $parameters[3]);
+            }
+
+            if ($data->first()) {
+                return true;
+            }
+            
+            return false;
+        });
+    }
+}
